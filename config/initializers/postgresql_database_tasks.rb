61560cc34bc504e74ada564510bbc4a3da91bbe4
grandtheft-spacehip <justdizon71@gmail.com>
2016-11-20 18:41:59 -0800

    :art: Adds comments for future reference

--- config/initializers/postgresql_database_tasks.rb
+++ config/initializers/postgresql_database_tasks.rb
@@ -9,3 +9,9 @@ module ActiveRecord
     end
   end
 end
+
+# https://www.krautcomputing.com/blog/2014/01/10/how-to-drop-your-postgres-database-with-rails-4/
+
+# Note 1: You need to run the db:drop Rake task with the environment option so the initializer is picked up:  rake environment db:drop
+
+# Note 2: As pointed out in the comments, the pg_stat_activity.pid column was called  pg_stat_activity.procpid in Postgres < 9.2. Make sure to replace it if you're working with an older version of Postgres.
